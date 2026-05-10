2dfdf883fa8ab57b0d902ef529a68f62dccbb06f
Darian Bruce <darianmariebruce@gmail.com>
2026-05-10 16:31:03 -0400

    finished program needs refactoring

--- Lab16_DarianMarieIsMe-1.py
+++ Lab16_DarianMarieIsMe-1.py
@@ -8,10 +8,6 @@ from pathlib import Path
 import csv
 import datetime
 
-dates: list = []
-
-unemployment_rates: list = []
-
 path: Path = Path('OHUR.csv')
 
 lines = path.read_text(encoding = 'utf-8').splitlines()
@@ -22,7 +18,34 @@ header_row = next(reader)
 
 print(header_row)
 
-for index, col_title in enumerate(header_row):
-    print(f'{index} {col_title}', end=' ')
+# for index, col_title in enumerate(header_row):
+    # print(f'{index} {col_title}', end=' ')
 
-print()
+# print()
+
+#processing info from file
+dates: list = []
+
+unemployment_rates: list = []
+
+for row in reader:
+    try: 
+        date = datetime.datetime.strptime(row[0], "%Y-%m-%d")
+        ohur: float = float(row[1])
+    except ValueError:
+        print(f"Could not convert data in row: {row}")
+
+    else:
+        dates.append(date)
+        unemployment_rates.append(ohur)
+
+#graph
+
+plt.style.use('dark_background')
+figure, graph = plt.subplots()
+plt.title("Ohio Unemployment (by Mont): 1976 - 2022")
+plt.xlabel("Date")
+plt.ylabel("Unemployment Rate")
+
+graph.plot(dates, unemployment_rates, color = 'blue')
+plt.show() 
