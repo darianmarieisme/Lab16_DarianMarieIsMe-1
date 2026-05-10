0b8dcce5e7392320efeb8f761a77d988c308e2c7
Darian Bruce <darianmariebruce@gmail.com>
2026-05-10 16:12:17 -0400

    reading text

--- Lab16_DarianMarieIsMe-1.py
+++ Lab16_DarianMarieIsMe-1.py
@@ -4,6 +4,25 @@ This program reads a CSV file and creates a line plot
 05/10/2026'''
 
 import matplotlib.pyplot as plt
+from pathlib import Path
 import csv
 import datetime
 
+dates: list = []
+
+unemployment_rates: list = []
+
+path: Path = Path('OHUR.csv')
+
+lines = path.read_text(encoding = 'utf-8').splitlines()
+
+reader = csv.reader(lines)
+
+header_row = next(reader)
+
+print(header_row)
+
+for index, col_title in enumerate(header_row):
+    print(f'{index} {col_title}', end=' ')
+
+print()
